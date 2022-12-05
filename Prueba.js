5adbbd1b3fc23728e0dacbb4e0315a99e106bf68
William <Williamlv1998@gmail.com>
2022-12-05 13:51:13 -0500

    push de tercera funcion

--- Prueba.js
+++ Prueba.js
@@ -1,6 +1,12 @@
 function Mensaje() {
     Logger.log("Hola mundo desde pc JP y william")
 }
+
 function Mensaje1() {
-    Logger.log("Hola")
+    alert("Hola");
+}
+
+
+function Mensaje2() {
+    alert("Hola tercer mensaje uso de git y clasp");
 }
